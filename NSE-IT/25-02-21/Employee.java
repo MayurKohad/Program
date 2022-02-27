880967770339b9db75568e1d6e719c96b70e1df3
MayurKohad <100231278+MayurKohad@users.noreply.github.com>
2022-02-28 00:59:39 +0530

    Add files via upload

---
+++ NSE-IT/25-02-21/Employee.java
@@ -0,0 +1,51 @@
+package Mayur;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+class Employee extends Object{
+    int empid;
+    String name;
+    String dept;
+    public Employee() {
+    }
+    public Employee(int empid, String name, String dept) {
+        super();
+        this.empid = empid;
+        this.name = name;
+        this.dept = dept;
+    }
+    @Override
+    public String toString() {
+        return "Employee [empid=" + empid + ", name=" + name + ", dept=" + dept + "]";
+    }
+
+}
+public class EmployeeListExample {
+
+    public static void main(String[] args) {
+        List<Employee> ar = new LinkedList();
+        Employee emp1 = new Employee(1001,"Kumar","IT");
+        Employee emp2 = new Employee(9021,"Sunil","Marketing");
+        Employee emp3 = new Employee(5329,"Babji","QA");
+        Employee emp4 = new Employee(2245,"John","Operations");
+        ar.add(emp1);
+        ar.add(emp2);
+        ar.add(emp3);
+        ar.add(emp4);
+        System.out.println(ar);
+        Iterator itr = ar.iterator();
+        while(itr.hasNext()) {
+            Employee emp = (Employee) itr.next();
+            System.out.println(emp.empid+" "+emp.name+" "+emp.dept);
+        }
+
+        System.out.println("By using for loop");
+        for(Employee e: ar) {
+            System.out.println(e.empid+" "+e.name+" "+e.dept);
+        }
+        ar.forEach(System.out::println);
+    }
+
+}
